5fb977ed6739ed9c8b211d218bf419f5eabab8f2
zandanyd <127864652+zandanyd@users.noreply.github.com>
2023-04-17 13:53:32 +0300

    Delete src directory

--- src/algorithms/mazeGenerators/IMazeGenerator.java
+++
@@ -1,6 +0,0 @@
-package algorithms.mazeGenerators;
-
-public interface IMazeGenerator {
-    Maze generate(int rowsNumber, int columnsNumber);
-    long measureAlgorithmTimeMillis(int rowsNumber, int columnsNumber);
-}
