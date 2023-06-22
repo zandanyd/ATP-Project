1ed1aa4da9e927a81562d699b2eea45e1aa19674
IdoDai <127848347+IdoDai@users.noreply.github.com>
2023-06-22 14:47:11 +0300

    Delete ATP-Project-PartB directory

--- ATP-Project-PartB/src/algorithms/search/ISearchingAlgorithm.java
+++
@@ -1,8 +0,0 @@
-package algorithms.search;
-
-public interface ISearchingAlgorithm  {
-    public Solution solve(ISearchable item);
-    public AState popOpenList();
-    public int getNumberOfNodesEvaluated();
-    public String getName();
-}
